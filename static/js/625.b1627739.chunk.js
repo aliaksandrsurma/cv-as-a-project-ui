31980447d0baa84dff51c1d7bdaff85db686d8da
aliaksandrsurma <aliaksandr.surma@gmail.com>
2024-01-15 10:13:52 +0300

    Updates

---
+++ static/js/625.b1627739.chunk.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkcv_as_a_project_ui=self.webpackChunkcv_as_a_project_ui||[]).push([[625],{9625:(a,c,e)=>{a.exports=e.p+"static/media/about.cdda53cd5e7747a7abc3.md"}}]);
--- static/js/625.cd2c363b.chunk.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkcv_as_a_project_ui=self.webpackChunkcv_as_a_project_ui||[]).push([[625],{625:(a,c,e)=>{a.exports=e.p+"static/media/about.cdda53cd5e7747a7abc3.md"}}]);
